241e1775e587858b4aa019037595e6e0fadf66ff
Ek <ekkapob@gmail.com>
2017-07-10 22:49:50 +0700

    add tempalte layout and data

--- template.js
+++ template.js
@@ -5,14 +5,15 @@ const handlerBarsExtension = '.hbs';
 const templatesDir = './templates';
 const partialsDir = templatesDir + '/partials';
 const distDir = './dist';
+const defaultLayoutFile = templatesDir + '/layout/default.hbs';
+const templateDataDir = templatesDir + '/data';
 
 function registerPartialFiles() {
   const partialFiles = Fs.readdirSync(partialsDir);
 
   partialFiles.forEach((file) => {
-    const fileName = file.split(handlerBarsExtension)[0];
-    const content = Fs.readFileSync(partialsDir + '/' + file, 'utf8');
-    Handlebars.registerPartial(fileName, content);
+    const fileName = getFileName(file, handlerBarsExtension);
+    registerPartial(fileName, partialsDir + '/' + file)
   });
   console.log('[OK] Partials registered');
 }
@@ -21,11 +22,13 @@ function generateTemplates() {
   let files = Fs.readdirSync(templatesDir);
   files = filterDirectoryAndJunk(files);
   files.forEach((file) => {
-    const fileName = file.split(handlerBarsExtension)[0];
-    const content = Fs.readFileSync(templatesDir + '/' + file,
-                                    { encoding: 'utf8' });
-    const template = Handlebars.compile(content);
-    const result = template();
+    const fileName = getFileName(file, handlerBarsExtension);
+    registerPartial('content', templatesDir + '/' + file)
+
+    const layoutContent = layoutTemplateContent();
+    const template = Handlebars.compile(layoutContent);
+    const data = contentData(file);
+    const result = (!data) ? template() : template(JSON.parse(data));
     Fs.writeFileSync(distDir + '/' + fileName + '.html',
                      result,
                      { encoding: 'utf8' });
@@ -39,6 +42,28 @@ function filterDirectoryAndJunk(files) {
     const notJunk = (file[0] != '.');
     return notDirectory && notJunk;
   });
+}
+
+function getFileName(file, extension) {
+  return file.split(extension)[0];
+}
+
+function registerPartial(name, filePath) {
+  const content = Fs.readFileSync(filePath, 'utf8');
+  Handlebars.registerPartial(name, content);
+}
+
+function layoutTemplateContent() {
+  return Fs.readFileSync(defaultLayoutFile, { encoding: 'utf8' });
+}
+
+function contentData(file) {
+  const fileName = getFileName(file, handlerBarsExtension);
+  const fileExists = Fs.existsSync(templateDataDir + '/' + fileName + '.json');
+  if (fileExists) {
+    return Fs.readFileSync(templateDataDir + '/' + fileName + '.json', 'utf8');
+  }
+  return undefined;
 }
 
 registerPartialFiles();
